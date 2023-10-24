3a565e7e04c4b9a0677b86a6655e3c12c619a7ff
Hafizz2 <106674504+Hafizz2@users.noreply.github.com>
2023-10-24 10:45:56 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
 function addNum(num) {
-    document.getElementById("numbox").value += num;
-  }
-  function operate(operator) {
-    document.getElementById("numbox").value += operator;
-    if(operator=="()"){
-        var temp = document.getElementById('numbox').value;
+  document.getElementById("numbox").value += num;
+}
+
+function operate(operator) {
+  const numbox = document.getElementById("numbox");
+  const lastChar = numbox.value.slice(-1); // Get the last character in the input
+
+  if (operator === "(" || operator === ")") {
+    // Check if the last character is an operator or an opening parenthesis
+    const isOperatorOrParenthesis = ["+", "-", "*", "/", "("].includes(
+      lastChar
+    );
+
+    if (operator === "(" && !isOperatorOrParenthesis) {
+      // Add a multiplication operator before an opening parenthesis if needed
+      numbox.value += "*(";
+    } else if (operator === ")" && !isOperatorOrParenthesis) {
+      // Add a closing parenthesis only if there is a matching opening parenthesis
+      const openingParenthesisCount = (numbox.value.match(/\(/g) || []).length;
+      const closingParenthesisCount = (numbox.value.match(/\)/g) || []).length;
+
+      if (openingParenthesisCount > closingParenthesisCount) {
+        numbox.value += operator;
+      }
     }
+  } else {
+    numbox.value += operator;
   }
-  function equal() {
-    const expression = document.getElementById("numbox").value;
-    try {
-      const result = eval(expression);
-      document.getElementById("numbox").value = result;
-    } catch (error) {
-      document.getElementById("numbox").value = "Syntax error";
-    }
+}
+
+function equal() {
+  const expression = document.getElementById("numbox").value;
+  try {
+    const result = eval(expression);
+    document.getElementById("numbox").value = result;
+  } catch (error) {
+    document.getElementById("numbox").value = "Syntax error";
   }
-  function clearAll() {
-    document.getElementById("numbox").value = "";
-  }
+}
+
+function clearAll() {
+  document.getElementById("numbox").value = "";
+}
